4488a8f5ed6ed938bfb9d478384ca2e418e9d26b
qianmoQ <shicheng@devlive.org>
2026-01-03 10:12:33 +0800

    feat: 支持 Php 语言环境安装

--- src-tauri/src/env_providers/mod.rs
+++ src-tauri/src/env_providers/mod.rs
@@ -1,10 +1,12 @@
 pub mod clojure;
 pub mod go;
 pub mod metadata;
+pub mod php;
 pub mod rust;
 pub mod scala;
 
 pub use clojure::ClojureEnvironmentProvider;
 pub use go::GoEnvironmentProvider;
+pub use php::PhpEnvironmentProvider;
 pub use rust::RustEnvironmentProvider;
 pub use scala::ScalaEnvironmentProvider;
